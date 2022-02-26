c8caed19e09ac1c1520856086a7e332a3f855d04
Allen <1942813644@qq.com>
2022-02-26 20:20:46 +0800

    搭建HTML结构

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,4 @@
 <template>
-  <div>导航栏 |
-    <router-link to="/">cx</router-link>
-    |
-    <router-link to="/xxx">cx2</router-link>
-  </div>
-  <hr/>
   <router-view/>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,15 @@ import {createApp} from 'vue';
 import App from './App.vue';
 import './index.css';
 import {createWebHashHistory, createRouter} from 'vue-router';
-import cx from './components/cx.vue';
-import cx2 from './components/cx2.vue';
+import Home from './views/Home.vue';
+import Doc from './views/Doc.vue';
 
 const history = createWebHashHistory();
 const router = createRouter({
   history: history,
   routes: [
-    {path: '/', component: cx},
-    {path: '/xxx', component: cx2}
+    {path: '/', component: Home},
+    {path: '/doc', component: Doc}
   ]
 });
 
