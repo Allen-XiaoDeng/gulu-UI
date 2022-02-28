e33743c040da41365c444b534bf283e3c6d2989d
Allen <1942813644@qq.com>
2022-02-28 09:37:56 +0800

    创建 Button 组件

---
+++ src/lib/Button.vue
@@ -0,0 +1,21 @@
+<template>
+  <div :size="size">
+    <button v-bind="rest">
+      你好
+    </button>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  inheritAttrs: false,
+  setup(props, context) {
+    const {size, ...rest} = context.attrs;
+    return {size, rest};
+  }
+};
+</script>
+<style lang="scss" scoped>
+div {
+  border: 1px solid red
+}
+</style>
