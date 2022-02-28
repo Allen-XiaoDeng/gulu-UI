f355811d3ebd1cf38658d0c568a758ff2a11b933
Allen <1942813644@qq.com>
2022-02-28 12:40:36 +0800

    加载中 css

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -3,6 +3,8 @@
   <button class="gulu-button"
           :class="classes"
           :disabled="disabled">
+    <span v-if="loading"
+          class="gulu-loadingIndicator"></span>
     <slot/>
   </button>
 </template>
@@ -25,6 +27,10 @@ export default {
       default: 'normal'
     },
     disabled: {
+      type: Boolean,
+      default: false
+    },
+    loading: {
       type: Boolean,
       default: false
     }
@@ -224,6 +230,27 @@ $orange: orange;
       cursor: not-allowed;
       color: $grey;
     }
+  }
+
+  > .gulu-loadingIndicator {
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px;
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: gulu-spin 1s infinite linear;
+  }
+}
+
+@keyframes gulu-spin {
+  0% {
+    transform: rotate(0deg)
+  }
+  100% {
+    transform: rotate(360deg)
   }
 }
 </style>
